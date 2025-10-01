dd36cc72e8d4bea3bce2d4a5a627656e2f7cfa45
Mancz_hdy <dimasalhamdy@gmail.com>
2025-10-01 21:59:28 +0700

    soal1.cpp

--- Tugas 1/soal1.cpp
+++ Tugas 1/soal1.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
+//subprogramnya
 void jumlahganjil(int *x, int y){
+   //inisialisasi nilai awal
     *x = 0;
+   
+   //perulangan untuk pengecekan dan operasi bilangannya
     for(int i = 1; i <= y; i++){
         if(i % 2 != 0){
             *x = *x + i;
@@ -12,9 +16,11 @@ void jumlahganjil(int *x, int y){
 
 int main(){
     int y, hasil;
-    
+
+   //masukkan y sebagai bil ganjilnya
     cout << "Nilai y : ";cin >> y;
-    
+
+   //pemanggilan subprogram
     jumlahganjil(&hasil, y);
     
     cout << "Hasil jumlah : " << hasil;
